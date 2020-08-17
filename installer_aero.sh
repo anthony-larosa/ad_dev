c53df6d9b165feab8579f5d6da8accf9c27670f5
anthony-larosa <anthony.d.larosa@nasa.gov>
2020-08-17 16:22:19 -0400

    Crontab edits for bugs

--- installer_aero.sh
+++ installer_aero.sh
@@ -65,19 +65,24 @@ cronjob1="@reboot sleep 300 && /home/$user_var/ad_dev/v5_cimel_connect/model5_co
 cronjob2="57 23 */2 * * /home/$user_var/ad_dev/status.sh"
 cronjob3="0 */3 * * * /home/$user_var/ad_dev/modem_dog.sh"
 
+{ crontab -l -u $user_var; echo "$cronjob1" } | crontab -u $user_var -
+{ crontab -l -u $user_var; echo "$cronjob2" } | crontab -u $user_var -
+{ crontab -l -u $user_var; echo "$cronjob3" } | crontab -u $user_var -
 
-(crontab -u $user_var -l; echo "$cronjob1" ) | crontab -u $user_var -
-(crontab -u $user_var -l; echo "$cronjob2" ) | crontab -u $user_var -
-(crontab -u $user_var -l; echo "$cronjob3" ) | crontab -u $user_var -
 
+mkdir /home/$user_var/logs #Make a log file directory
 
-mkdir /home/$user_var/logs
-cp -r $PWD /home/$user_var
+cp -r $PWD /home/$user_var #Copy the programs from current user to new user
+
 mv /home/$user_var/ad_dev/holoConn.service /etc/systemd/system/
+
 sleep 1
 echo "Enabling services..."
 sleep 1
+
 systemctl enable holoConn.service
+systemctl status holoConn.service
+
 cd /home/$user_var/ad_dev/v5_cimel_connect
 echo "Compiling cimel connect..."
 cc -g -o multi_https_connect multi_connect.c my_com_port.c aero_time.c -lm -L /usr/lib/arm-linux-gnueabihf/ -lcurl
