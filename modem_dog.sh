a80f83809eb4437e06d8285a4c14b0f39ed91bc8
anthony-larosa <anthony.d.larosa@nasa.gov>
2021-01-20 15:47:49 +0000

    replaced hardcoded user with

--- modem_dog.sh
+++ modem_dog.sh
@@ -9,6 +9,6 @@ if getscript "model5_connect" >/dev/null; then
 	echo "Model 5 connect is running at ${now}" >> $HOME/logs/connection.log
 else
 	echo "Model 5 connect was not running at ${now}." >> $HOME/logs/connection.log
-	sudo /home/RPiz_1/ad_dev/v5_cimel_connect/model5_connect USB0
+	sudo /home/$USER/ad_dev/v5_cimel_connect/model5_connect USB0
 fi
 echo "Finished checking if Cimel Connect was running"
