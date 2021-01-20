99309892dda7ef1f0af573bd7cb2594f52c42219
anthony-larosa <anthony.d.larosa@nasa.gov>
2021-01-20 15:06:24 +0000

    error logging

--- modem_dog.sh
+++ modem_dog.sh
@@ -8,7 +8,7 @@ getscript() {
 if getscript "model5_connect_silent" >/dev/null; then
 	echo "Model 5 connect is running at ${now}" >> $HOME/logs/connection.log
 else
-	echo "Model 5 connect is not running at ${now}" >> $HOME/logs/connection.log
+	echo "Model 5 connect was not running at ${now}." >> $HOME/logs/connection.log
 	sudo /home/RPiz_1/ad_dev/v5_cimel_connect/model5_connect USB0
 fi
 echo "Finished checking if Cimel Connect was running"
