25bbb7e8105348917f679c0574fe21d60343a774
anthony-larosa <anthony.d.larosa@nasa.gov>
2021-03-10 22:21:33 +0000

    modem_dog.sh

--- modem_dog.sh
+++ modem_dog.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 now=$(date)
-
+counter=0
 getscript() {
   pgrep -lf ".[ /]$1( |\$)"
 }
-if getscript "model5_connect" >/dev/null; then
-	echo "Model 5 connect is running at ${now}" >> $HOME/logs/connection.log
-else
-	echo "Model 5 connect was not running at ${now}." >> $HOME/logs/connection.log
-	/home/RPiz_2/ad_dev/v5_cimel_connect/model5_connect USB0
-fi
-echo "Finished checking if Cimel Connect was running"
+while [ $counter -lt 5 ]; do
+	if getscript "model5_connect" >/dev/null; then
+		echo "Model 5 connect is running at ${now}"
+	else
+		echo "Model 5 connect was not running at ${now}." >> $HOME/logs/connection.log
+		counter=$((counter+1))
+		/home/$USER/ad_dev/v5_cimel_connect/model5_connect USB0
+		sleep 30m
+	fi
+done
+echo "Reached counter at ${now} and performed reboot" >> $HOME/logs/connection.log
+sudo reboot
